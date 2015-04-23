015bfcf3f64b4487bf915b4ac0ab8a4d2f5d823a
Janus Schmidt <janusschmidt@gmail.com>
2015-04-23 23:06:54 +0200

    Try to fix prerelease semver versioning

--- Autohasher/Properties/AssemblyInfo.cs
+++ Autohasher/Properties/AssemblyInfo.cs
@@ -38,6 +38,7 @@ using System.Security;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("")]
 
 
 
